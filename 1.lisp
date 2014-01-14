29ea2765698bb85e521278d2e72bed8121f40159
Christoffer Pedersen <stofferpedersen@gmail.com>
2014-01-14 22:38:42 +0100

    1 done

--- 1.lisp
+++ 1.lisp
@@ -29,19 +29,28 @@
       (= 0 (mod n 5))))
 
 ;; 2)
+(defun list-to-n (n) 
+  (reverse 
+    (let (v) 
+      (dotimes (i n v) 
+        (setq v (cons i v))))))
 
+;; 3) and 4)
 (defun result ()
-  (multipleOf3Or5 5))
+  (apply '+ (remove-if-not 'multipleOf3Or5 (list-to-n 1000))))
 
 (defun test (description expected actual)
-  (format t "~S: ~S ~%" description (eq expected actual)))
+  (format t "~S: ~S ~%" description (equal expected actual)))
 
 (when *test* T
   (progn 
     (test "3 is multiple of 3 or 5" T (multipleOf3Or5 3))
     (test "5 is multiple of 3 or 5" T (multipleOf3Or5 5))
     (test "9 is multiple of 3 or 5" T (multipleOf3Or5 9))
-    (test "8 is not multiple of 3 or 5" NIL (multipleOf3Or5 8))))
+    (test "8 is not multiple of 3 or 5" NIL (multipleOf3Or5 8))
+    (test "list-to-n can build a list like [0..n]" 
+          '(0 1 2) (list-to-n 3))
+    ))
 
 (when *run* T
     (format t "~S" (result)))
